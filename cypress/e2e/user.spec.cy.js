4df822fba58471ef6a4045bb9712e0ff05f1d49c
Rodrigo Silva <rodrigo_souzaa130@hotmail.com>
2025-10-24 19:12:55 -0300

    add random values on user spec chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,34 +4,26 @@ import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 
+  const Chance = require('chance');
+
   const loginPage = new LoginPage()
   const dashboardPage = new DashboardPage()
   const menuPage = new MenuPage()
   const myInfoPage = new MyInfoPage()
+  const chance = new Chance()
 
-describe('Orange HRM - Tests', () => {
+describe('User Orange HRM - Tests', () => {
 
-  const selectorsList = {
-    
-  }
-
-  it.only('User Info Update - Success', () => {
+  it('User Info Update - Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
     dashboardPage.checkDashboardPage()
     menuPage.accessMyInfo()
-    myInfoPage.fillPersonalDetails('First Name','Last Name')
-    myInfoPage.fillEmployeeDetails('EmployId','OtherId','777','2025-10-07')
+    myInfoPage.fillPersonalDetails(chance.first(),chance.last())
+    myInfoPage.fillEmployeeDetails(chance.word(),chance.second(),'111','2025-10-24')
     myInfoPage.fillStatus()
     myInfoPage.saveForm()
 
   })
   
-  it('Login - Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userFail.username)
-    cy.get(selectorsList.passwordField).type(userData.userFail.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.get(selectorsList.wrongCredentialAlert)
-  })
 })
